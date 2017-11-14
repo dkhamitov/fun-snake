d9bbb53c92940d24097b604903144a3d4d2ef2ca
dkhamitov <khamitov.dm@gmail.com>
2017-11-14 21:33:58 +0300

    Make SnakeTest behavior-driven

--- src/test/kotlin/d/kh/fan/snake/SnakeTest.kt
+++ src/test/kotlin/d/kh/fan/snake/SnakeTest.kt
@@ -1,36 +1,56 @@
 package d.kh.fan.snake
 
 import io.kotlintest.matchers.shouldBe
-import io.kotlintest.specs.StringSpec
+import io.kotlintest.specs.BehaviorSpec
 
-class SnakeTest : StringSpec() {
-    init {
-        "Y coordinate of one-link snake's head should change to Y - 1 when moving UP" {
-            val snake = Snake()
+class SnakeTest : BehaviorSpec({
+    Given("a one-link snake") {
+        val (x, y, snake) = makeSnake()
+        When("when I move it UP") {
             snake.up()
-            snake.head().x shouldBe 0
-            snake.head().y shouldBe -1
-        }
-
-        "Y coordinate of one-link snake's head should change to Y + 1 when moving DOWN" {
-            val snake = Snake()
-            snake.down()
-            snake.head().x shouldBe 0
-            snake.head().y shouldBe 1
-        }
-
-        "X coordinate of one-link snake's head should change to X - 1 when moving LEFT" {
-            val snake = Snake()
-            snake.left()
-            snake.head().x shouldBe -1
-            snake.head().y shouldBe 0
-        }
-
-        "X coordinate of one-link snake's head should change to X + 1 when moving RIGHT" {
-            val snake = Snake()
-            snake.right()
-            snake.head().x shouldBe 1
-            snake.head().y shouldBe 0
+            Then("it should change Y to Y - 1") {
+                snake.head().x shouldBe x
+                snake.head().y shouldBe y - 1
+            }
         }
     }
+
+    Given("a one-link snake") {
+        val (x, y, snake) = makeSnake()
+        When("when I move it DOWN") {
+            snake.down()
+            Then("it should change Y to Y + 1") {
+                snake.head().x shouldBe x
+                snake.head().y shouldBe y + 1
+            }
+        }
+    }
+
+    Given("a one-link snake") {
+        val (x, y, snake) = makeSnake()
+        When("when I move it LEFT") {
+            snake.left()
+            Then("it should change X to X - 1") {
+                snake.head().x shouldBe x - 1
+                snake.head().y shouldBe y
+            }
+        }
+    }
+
+    Given("a one-link snake") {
+        val (x, y, snake) = makeSnake()
+        When("when I move it RIGHT") {
+            snake.right()
+            Then("it should change X to X + 1") {
+                snake.head().x shouldBe x + 1
+                snake.head().y shouldBe y
+            }
+        }
+    }
+})
+
+fun makeSnake(): Triple<Int, Int, Snake> {
+    val head = Point(0, 0)
+    val snake = Snake(listOf(head))
+    return Triple(head.x, head.y, snake)
 }
