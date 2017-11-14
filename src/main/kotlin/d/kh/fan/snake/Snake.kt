65165b083e30520bf2b78c0529f99daf16943ba7
dkhamitov <khamitov.dm@gmail.com>
2017-11-15 02:03:04 +0300

    Simplify validation on bumping snake into itself

--- src/main/kotlin/d/kh/fan/snake/Snake.kt
+++ src/main/kotlin/d/kh/fan/snake/Snake.kt
@@ -1,7 +1,5 @@
 package d.kh.fan.snake
 
-import d.kh.fan.snake.Direction.*
-
 class Snake(chain: List<Point> = mutableListOf(Point(0, 0))) : Iterable<Point> {
     private val chain: MutableList<Point> = chain.toMutableList()
 
@@ -9,48 +7,31 @@ class Snake(chain: List<Point> = mutableListOf(Point(0, 0))) : Iterable<Point> {
 
     fun tail() = chain.last()
 
-    fun size() = chain.size
+    fun length() = chain.size
 
     fun up() {
-        checkStepBack(UP)
         move(Point(chain.first().x, chain.first().y - 1))
     }
 
     fun down() {
-        checkStepBack(DOWN)
         move(Point(chain.first().x, chain.first().y + 1))
     }
 
     fun left() {
-        checkStepBack(LEFT)
         move(Point(chain.first().x - 1, chain.first().y))
     }
 
     fun right() {
-        checkStepBack(RIGHT)
         move(Point(chain.first().x + 1, chain.first().y))
     }
 
     override fun iterator() = chain.iterator()
 
-    private fun checkStepBack(direction: Direction) {
-        if (chain.size == 1) {
-            return
-        }
-        val stepBack = when (direction) {
-            UP -> chain.first().x == chain.second().x && chain.first().y > chain.second().y
-            DOWN -> chain.first().x == chain.second().x && chain.first().y < chain.second().y
-            LEFT -> chain.first().y == chain.second().y && chain.first().x > chain.second().x
-            RIGHT -> chain.first().y == chain.second().y && chain.first().x < chain.second().x
-        }
-        if (stepBack) {
-            throw IllegalStateException("Step $direction not allowed in current state")
-        }
-    }
-
     private fun move(head: Point) {
+        if (length() > 1 && (head == chain.second() || chain.dropLast(1).contains(head))) {
+            throw IllegalStateException("No bump allowed")
+        }
         chain.removeAt(chain.lastIndex)
-        chain.forEach { point -> if (head == point) throw IllegalStateException("No bump allowed") }
         chain.add(0, head)
     }
 }
